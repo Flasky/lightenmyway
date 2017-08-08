e867815b6628d01a24c8c0bcf3e5216514ccbe51
Yidi Zhu <yidizhu@halitegames.com>
2017-08-08 22:16:46 +0800

    New assets and Open video

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour {
     }
 
 	public void StartGame() {
-        SceneManager.LoadScene("Select Menu");
+        SceneManager.LoadScene("Video");
     }
 
     public void LoadLevel(int level) {
---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour {
+
+	public VideoPlayer videoPlayer;
+
+    void Awake() {
+        GameObject.Find("GameManager").GetComponent<AudioSource>().Pause();
+        videoPlayer.Play();
+        StartCoroutine(CheckStopCoroutine());
+    }
+
+    IEnumerator CheckStopCoroutine() {
+        yield return new WaitForSeconds(50f);
+        while (true) {
+            if (!videoPlayer.isPlaying) {
+                GameObject.Find("GameManager").GetComponent<AudioSource>().Play();
+                SceneManager.LoadScene("Select Menu");
+            }
+            yield return null;
+        }
+    }
+
+
+
+}
